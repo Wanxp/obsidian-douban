c1e25c630cbd15629d70e3d364d587754c479c5e
wanxp <977741432@qq.com>
2022-11-20 11:25:32 +0800

    fix #24 show wrong chinese name

--- src/org/wanxp/douban/data/handler/DoubanAbstractLoadHandler.ts
+++ src/org/wanxp/douban/data/handler/DoubanAbstractLoadHandler.ts
@@ -189,11 +189,11 @@ export default abstract class DoubanAbstractLoadHandler<T extends DoubanSubject>
 		let regValue: RegExpExecArray;
 		switch (personNameMode) {
 			case PersonNameMode.CH_NAME:
-				regValue = /[\u4e00-\u9fa50-9\. \:\u3002|\uff1f|\uff01|\uff0c|\u3001|\uff1b|\uff1a|\u201c|\u201d|\u2018|\u2019|\uff08|\uff09|\u300a|\u300b|\u3008|\u3009|\u3010|\u3011|\u300e|\u300f|\u300c|\u300d|\ufe43|\ufe44|\u3014|\u3015|\u2026|\u2014|\uff5e|\ufe4f|\uffe5]{2,20}/g.exec(name);
+				regValue = /[\u4e00-\u9fa50-9\. ·\:\u3002|\uff1f|\uff01|\uff0c|\u3001|\uff1b|\uff1a|\u201c|\u201d|\u2018|\u2019|\uff08|\uff09|\u300a|\u300b|\u3008|\u3009|\u3010|\u3011|\u300e|\u300f|\u300c|\u300d|\ufe43|\ufe44|\u3014|\u3015|\u2026|\u2014|\uff5e|\ufe4f|\uffe5\(\)]{2,}/g.exec(name);
 				resultName = regValue ? regValue[0] : name;
 				break;
 			case PersonNameMode.EN_NAME:
-				regValue = /[0-9a-zA-Z.\s-]{2,50}/g.exec(name);
+				regValue = /[0-9a-zA-Z.\s-:]{2,}/g.exec(name);
 				resultName = regValue ? regValue[0] : name;
 				break;
 			default:
